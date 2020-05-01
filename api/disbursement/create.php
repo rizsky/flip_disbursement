324354c7c32ad76700389792575aafa5c98c6a9e
rizsky <rizskysky@gmail.com>
2020-05-01 13:59:27 +0700

    feat: added feature for disbursement request and added response to db

---
+++ api/disbursement/create.php
@@ -0,0 +1,87 @@
+<?php
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/x-www-form-urlencoded');
+  header('Access-Control-Allow-Methods: POST');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
+
+  include_once '../../config/Database.php';
+  include_once '../../models/Disbursement.php';
+
+
+//get request body
+$data = file_get_contents("php://input");
+
+//init request to 3rd party 
+$ch = curl_init(); 
+$username = "SHl6aW9ZN0xQNlpvTzduVFlLYkc4TzRJU2t5V25YMUp2QUVWQWh0V0tadW1vb0N6cXA0MTo";
+
+$headers = array(
+    'Content-Type: application/x-www-form-urlencoded',
+    'Authorization: Basic '. ($username),
+    'Access-Control-Allow-Methods: POST',
+);    
+    curl_setopt_array($ch, array(
+    CURLOPT_URL => 'https://nextar.flip.id/disburse',
+    CURLOPT_HTTPHEADER => $headers,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => $data,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FAILONERROR => true,
+));
+$output = curl_exec($ch);
+if (!curl_exec($ch)) {
+    // if curl_exec() returned false and thus failed
+    echo json_encode(
+        array(
+        'err' => curl_error($ch),
+        'message' => 'something went wrong'
+        )
+      );
+    exit();
+  
+}    
+curl_close($ch);
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
+
+  // Instantiate blog post object
+  $disbursement = new Disbursement($db);
+
+  // Get raw posted data
+  $response = json_decode($output);
+
+  if ($response->time_served == "0000-00-00 00:00:00") {
+      $response->time_served = "1000-01-01 00:00:00";
+  }
+ 
+  $disbursement->id = $response->id;
+  $disbursement->amount = $response->amount;
+  $disbursement->status = $response->status;
+  $disbursement->timestamp = $response->timestamp;
+  $disbursement->bank_code = $response->bank_code;
+  $disbursement->account_number = $response->account_number;
+  $disbursement->beneficiary_name = $response->beneficiary_name;
+  $disbursement->remark = $response->remark;
+  $disbursement->receipt = $response->receipt;
+  $disbursement->time_served = $response->time_served;
+  $disbursement->fee = $response->fee;
+  $disbursement->name = $response->name;
+
+  // Create Category
+  if($disbursement->create()) {
+    echo json_encode(
+      array(
+      'data' => $response,
+      'message' => 'Disbursement Created'
+      )
+    );
+  } else {
+    echo json_encode(
+      array(
+          'data' => null,
+          'message' => 'Disbursement Not Created'
+          )
+    );
+  }
---
+++ models/Disbursement.php
@@ -0,0 +1,93 @@
+<?php
+  class Disbursement {
+    // DB Stuff
+    private $conn;
+    private $table = 'disbursement';
+
+    // Properties
+    public $id;
+    public $amount;
+    public $status;
+    public $timestamp;
+    public $bank_code;
+    public $account_number;
+    public $beneficiary_name;
+    public $remark;
+    public $receipt;
+    public $time_served;
+    public $fee;
+    // Constructor with DB
+    public function __construct($db) {
+      $this->conn = $db;
+    }
+
+  // Create Disbursement
+  public function create() {
+      
+  // Create query]
+  $query = 'INSERT INTO ' . $this->table . ' 
+  SET id = :id, amount = :amount, status = :status, timestamp = :timestamp,
+  bank_code = :bank_code, account_number = :account_number, beneficiary_name = :beneficiary_name, remark = :remark,
+  receipt = :receipt, time_served = :time_served, fee = :fee'
+  ;
+
+  // Prepare Statement
+  $stmt = $this->conn->prepare($query);
+
+  // Bind data
+  $stmt-> bindParam(':id', $this->id);
+  $stmt-> bindParam(':amount', $this->amount);
+  $stmt-> bindParam(':status', $this->status);
+  $stmt-> bindParam(':timestamp', $this->timestamp);
+  $stmt-> bindParam(':bank_code', $this->bank_code);
+  $stmt-> bindParam(':account_number', $this->account_number);
+  $stmt-> bindParam(':beneficiary_name', $this->beneficiary_name);
+  $stmt-> bindParam(':remark', $this->remark);
+  $stmt-> bindParam(':receipt', $this->receipt);
+  $stmt-> bindParam(':time_served', $this->time_served);
+  $stmt-> bindParam(':fee', $this->fee);
+
+  // Execute query
+  if($stmt->execute()) {
+    return true;
+  }
+
+  // Print error if something goes wrong
+  printf("Error: $s.\n", $stmt->error);
+
+  return false;
+  }
+
+  // Update Category
+  public function update() {
+    // Create Query
+    $query = 'UPDATE ' .
+      $this->table . '
+    SET
+      name = :name
+      WHERE
+      id = :id';
+
+  // Prepare Statement
+  $stmt = $this->conn->prepare($query);
+
+  // Clean data
+  $this->name = htmlspecialchars(strip_tags($this->name));
+  $this->id = htmlspecialchars(strip_tags($this->id));
+
+  // Bind data
+  $stmt-> bindParam(':name', $this->name);
+  $stmt-> bindParam(':id', $this->id);
+
+  // Execute query
+  if($stmt->execute()) {
+    return true;
+  }
+
+  // Print error if something goes wrong
+  printf("Error: $s.\n", $stmt->error);
+
+  return false;
+  }
+
+  }
